ef68546ca9b23b736dde67070ab856422c63a828
erikcaffrey <erikcaffrey10@gmail.com>
2016-02-09 21:21:16 -0600

    Initial commit

---
+++ app/src/main/java/gdg/androidtitltan/com/androidinight_9/ImageViewCustom.java
@@ -0,0 +1,16 @@
+package gdg.androidtitltan.com.androidinight_9;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Jhordan on 09/02/16.
+ */
+public class ImageViewCustom extends View {
+
+    public ImageViewCustom(Context context) {
+        super(context);
+    }
+
+
+}
